94eb022ceba06bf72f140baf04e44aad292c4df7
Atul70911 <atul7091143875@gmail.com>
2026-01-24 06:24:52 +0530

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/POLLING-WEBSITE/", // <-- put your repo name exactly here
+  base: "/Polling-Website/", 
 });
