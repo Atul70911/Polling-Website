7a1cd8e525f521f5a4de150b70f5b5adb5d18d69
Atul70911 <atul7091143875@gmail.com>
2026-01-25 01:02:12 +0530

    cors-added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
+var cors = require('cors')
 const express = require('express')
-const app = express()
 const PORT = 3000
 
+
+
+
+const app = express()
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
