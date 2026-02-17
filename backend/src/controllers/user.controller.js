a07182c706fe56ef06cb6e7c8217c8f32ef85be9
Atul70911 <atul7091143875@gmail.com>
2026-02-18 00:44:17 +0530

    user-controller-added

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,10 +1,23 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.models.js";
 
 const registerUser= asyncHandler(
     async (req,res)=>{
-        res.status(200).json({
-            message:"ok",
+        const {name,username,email,password}=req.body;
+        if ([name,username,email,password].some((field)=>field?.trim()==="")) {
+            throw new ApiError(400,"All fields are required");
+        }
+        const existUser=User.findOne({
+            $or:[{email}]
         })
+        if(existUser){
+            throw new ApiError(409,"User Already Exists");
+        }
+
+        res.files?.avatar
+
+
     }
 )
 
