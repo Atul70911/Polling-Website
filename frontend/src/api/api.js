dee345f7807e16fa94590c9a76d42582cb434883
Atul70911 <atul7091143875@gmail.com>
2026-02-22 17:36:43 +0530

    url-again-updated

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://polling-website-28iv.onrender.com/api/v1",
+  baseURL:"https://polling-website-28iv.onrender.com/api/v1",
   withCredentials: true, // required if backend uses httpOnly cookies
 });
